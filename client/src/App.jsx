4b4e98f65960da2960c4c98741d32f36b4a27f09
sahil gupta <8278087870sahil@gmail>
2024-07-03 00:30:42 +0530

    feat: add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import TextEditor from "./components/TextEditor/TextEditor";
 import { Routes, Navigate, Route } from "react-router-dom";
 import { v4 as uuidV4 } from "uuid";
+
+import Header from "./components/Header/header";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to={`/document/${uuidV4()}`} />} />
-      <Route path="/document/:id" element={<TextEditor />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to={`/document/${uuidV4()}`} />} />
+        <Route path="/document/:id" element={<TextEditor />} />
+      </Routes>
+    </>
   );
 }
 
