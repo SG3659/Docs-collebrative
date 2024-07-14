9b09376ac9fabfbc962c65366d711fd919c2d048
sahil gupta <8278087870sahil@gmail>
2024-07-14 19:56:10 +0530

    feat:homeheader css to tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        google: [
+          '"Google Sans"',
+          "Roboto",
+          "RobotoDraft",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
   },
   plugins: [],
-}
+};
