c7f14e17775468735cf0eee584770fcace5095e6
sahil gupta <8278087870sahil@gmail>
2024-08-01 12:47:47 +0530

    feat: make lazy all pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -6,12 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import Spinner from "../src/components/Spinner/Spinner.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <Suspense fallback={<Spinner />}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Suspense>
+
       <Toaster position="top-rigth" reverseOrder={false} />
     </BrowserRouter>
   </React.StrictMode>
